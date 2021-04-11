db8806c3acb57fea6735dd465494129c7f68a698
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-11 14:29:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+// First, we're going to create four arrays of pets. Define 4 `const` called:
+// `append`, `prepend`, `removeLast` and `removeFirst`, and set all to an initial
+// value of `["Milo", "Otis", "Garfield"]`. These are the `array`s that we'll work
+// with for each exercise.
+// const pets = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"];
+append.push('Odie');
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.unshift('Odie');
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop();
+const removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift();
